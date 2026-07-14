a8d2c9833a788588c7283467ffaa018db974c192
robert thornton <robert.thornton92@gmail.com>
2026-07-14 16:05:29 +0200

    chore: export new interface that extends mongoose document - providing access to mogoose specific default fields

--- database/question.model.ts
+++ database/question.model.ts
@@ -1,4 +1,4 @@
-import { Schema, model, models, Types } from "mongoose";
+import { Schema, model, models, Types, Document } from "mongoose";
 
 export interface IQuestion {
   title: string;
@@ -10,6 +10,8 @@ export interface IQuestion {
   tags: Types.ObjectId[];
   author: Types.ObjectId;
 }
+
+export interface IQuestionDoc extends IQuestion, Document {}
 
 const QuestionSchema = new Schema<IQuestion>(
   {
