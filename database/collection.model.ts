2df634b4ca3d3a2aad52d951192b1ba8ff0f9504
robert thornton <robert.thornton92@gmail.com>
2026-07-14 11:22:01 +0200

    database (model): created collection model

---
+++ database/collection.model.ts
@@ -0,0 +1,18 @@
+import { Schema, model, models, Types } from "mongoose";
+
+export interface ICollection {
+  author: Types.ObjectId;
+  question: Types.ObjectId;
+}
+
+const CollectionSchema = new Schema<ICollection>(
+  {
+    author: { Type: Schema.Types.ObjectId, ref: " User", required: true },
+    question: { Type: Schema.Types.ObjectId, ref: "Question", required: true },
+  },
+  { timestamps: true }
+);
+
+const Collection = models?.Collection || model<ICollection>("Collection", CollectionSchema);
+
+export default Collection;
