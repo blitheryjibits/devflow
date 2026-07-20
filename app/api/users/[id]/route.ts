202baa9493a8549014a97e8097563ebef5af3d41
robert thornton <robert.thornton92@gmail.com>
2026-07-20 23:46:18 +0200

    backend (route): implemented getById route for users

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,15 +1,17 @@
 import User from "@/database/user.model";
 import handleError from "@/lib/handlers/error";
+import { NotFoundError } from "@/lib/https-errors";
 import dbConnect from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(_: Request, { params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  if (!id) throw new NotFoundError("User");
   try {
-    const { id } = await params;
     await dbConnect();
+    const user = await User.findById(id);
+    if (!user) throw new NotFoundError("User");
 
-    const user = User.findOne({ id });
-    if (!user) return NextResponse.json({ success: false }, { status: 400 });
     return NextResponse.json({ success: true, data: user }, { status: 200 });
   } catch (error) {
     return handleError(error, "api") as APIErrorResponse;
