edcfb65d5affa37b179cf01f21b9309a962f3835
robert thornton <robert.thornton92@gmail.com>
2026-07-21 21:45:16 +0200

    backend (route): add get account via providerAccountId

---
+++ app/api/accounts/provider/route.ts
@@ -0,0 +1,25 @@
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/https-errors";
+import { AccountSchema } from "@/lib/vallidations";
+import Account from "@/database/account.model";
+import { NextResponse } from "next/server";
+import dbConnect from "@/lib/mongoose";
+
+export async function POST(request: Request) {
+  const { providerAccountId } = await request.json();
+
+  try {
+    const validatedData = AccountSchema.partial().safeParse({ providerAccountId });
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    await dbConnect();
+    const account = await Account.findOne({ providerAccountId });
+    if (!account) throw new NotFoundError("Account");
+
+    return NextResponse.json({ success: true, data: account }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
