f64028c3eed900da5d2079593375553c382e05c2
robert thornton <robert.thornton92@gmail.com>
2026-08-02 16:25:56 +0300

    backend (auth): implement email and password signin functionality

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 import AuthForm from "@/components/forms/AuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
 import { SignInSchema } from "@/lib/vallidations";
+
 const SignIn = () => {
 	return (
 		<AuthForm
 			formType="SIGN_IN"
 			schema={SignInSchema}
 			defaultValues={{ email: "", password: "" }}
-			onSubmit={(data) => Promise.resolve({ success: true, data })}
+			onSubmit={signInWithCredentials}
 		/>
 	);
 };
