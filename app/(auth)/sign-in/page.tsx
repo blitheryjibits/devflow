588b10d476d21ebc91eff028a7a246e12ab5f488
robert thornton <robert.thornton92@gmail.com>
2026-08-03 13:18:45 +0300

    chore (refactor): fixed typo in folder name for lib/validations

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import AuthForm from "@/components/forms/AuthForm";
 import { signInWithCredentials } from "@/lib/actions/auth.action";
-import { SignInSchema } from "@/lib/vallidations";
+import { SignInSchema } from "@/lib/validations";
 
 const SignIn = () => {
 	return (
