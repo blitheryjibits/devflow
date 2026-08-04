c2c2c2ae59d8e6ddd79ecbb53397ac79aa4736a0
robert thornton <robert.thornton92@gmail.com>
2026-08-04 13:43:00 +0300

    chore (ui): add the ID of the question in the title

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -1,7 +1,7 @@
 const QuestionDetails = async ({ params }: RouteParams) => {
 	const { id } = await params;
 
-	return <div>Question Content</div>;
+	return <div>Question Content: {id}</div>;
 };
 
 export default QuestionDetails;
