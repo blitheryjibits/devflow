92218caa4542c8c343f36f861194c27708cdd905
robert thornton <robert.thornton92@gmail.com>
2026-08-05 14:03:24 +0300

    debug (routes): add / to front of route strings

--- constants/route.ts
+++ constants/route.ts
@@ -3,9 +3,9 @@ const ROUTES = {
 	SIGN_IN: "/sign-in",
 	SIGN_UP: "/sign-up",
 	ASK_QUESTION: "/ask-question",
-	QUESTION: (id: string) => `questions/${id}`,
-	PROFILE: (id: string) => `profile/${id}`,
-	TAGS: (id: string) => `tags/${id}`,
+	QUESTION: (id: string) => `/questions/${id}`,
+	PROFILE: (id: string) => `/profile/${id}`,
+	TAGS: (id: string) => `/tags/${id}`,
 	SIGNIN_WITH_OAUTH: "signin-with-oauth",
 };
 
